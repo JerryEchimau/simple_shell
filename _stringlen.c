36b921402f4c0199b3a1df4d8d1aaf2366380367
selejr10 <selebest10@gmail.com>
2023-11-08 09:08:12 -0800

    _stringlen.c

---
+++ _stringlen.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+* _stringlen - Program returns the length of a string.
+* @string: string array to be counted.
+* Return: Length of the string.
+*/
+
+int _stringlen(char *string)
+{
+	int length_counter = 0;
+
+	while (*string != '\0')
+	{
+		length_counter += 1;
+
+		string += 1;
+	}
+
+	return (length_counter);
+}
